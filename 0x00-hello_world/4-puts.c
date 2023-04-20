08a516b66f6e90d76fa0871539c9f1519c1a13c3
nourister <nouristerjuma@gmail.com>
2023-04-20 01:47:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- *main - main entry point
+ * main - main entry point
+ * Return: always 0 for (success output)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/**
-	 *return 0 :0 for success
-	 */
 	return (0);
 }
