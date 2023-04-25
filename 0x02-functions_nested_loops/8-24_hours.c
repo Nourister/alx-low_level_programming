eb19a17d49683f6f04ea2216874134fa3e0d3bc8
nourister <nouristerjuma@gmail.com>
2023-04-25 11:40:52 -0700

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,33 +2,30 @@
 
 /**
  * jack_bauer - print the minutes of a day
- *
+ * starting from 00:00 to 23:59
  * Return: no return
  */
 
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int a, b;
 
-	for (a = 0; a <= 2; a++)
+	a = 0;
+
+	while (a < 24)
 	{
-	for (b = 0; b <= 9; b++)
+	b = 0;
+	while (b < 60)
 	{
-	for (c = 0; c <= 5; c++)
-	{
-	for (d = 0; d <= 9; d++)
-	{
-	for (a >= 2 && b >= 4)
-	break;
-	_putchar(a + 48);
-	_putchar(b + 48);
-	_putchar(58);
-	_putchar(c + 48);
-	_putchar(d + 48);
+	_putchar((a / 10) + '0');
+	_putchar((a % 10) + '0');
+	_putchar(':');
+	_putchar((b / 10) + '0');
+	_putchar((b % 10) + '0');
 	_putchar('\n');
+	a++;
 	}
-	}
-	}
+	b++;
 	}
 }
 
