26f423aec9154c0e3ee0ad0eb83febe053dbd801
nourister <nouristerjuma@gmail.com>
2023-04-25 03:46:21 -0700

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  prints the alphabet x10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int n = 0;
+
+	while (n <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	n++;
+	}
+}
