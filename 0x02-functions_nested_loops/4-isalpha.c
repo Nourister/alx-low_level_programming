9fda7124b6414b20ac3db693830d83922c8747d3
nourister <nouristerjuma@gmail.com>
2023-04-25 05:29:19 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isalpha_ - checks for alphabetic character
+ * _isalpha - checks for alphabetic character
  * @c: The character to be checked
  * Return: 1 for lowercase or upeercase or 0 for anthing else
  */
