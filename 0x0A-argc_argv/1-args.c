9edabd8310fe15a8b250c954be5150f933040aad
nourister <nouristerjuma@gmail.com>
2023-05-12 01:18:03 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,10 +10,9 @@
 
 int main(int argc, char *argv[])
 {
-	int count;
+	(void) argv;
 
-	for (count = 0; count < argc; count++)
-		printf("argv[%2d]: %s\n", count, argv[count]);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
