36607105c18f9b315de93a7ffe6258128fd0c6ff
nourister <nouristerjuma@gmail.com>
2023-05-12 04:52:03 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: counts arguments
+ * @argv: an array if string
+ * Return: an integer
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned int j, sum;
+	char *c;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+	{
+		c = argv[i];
+
+		for (j = 0; j > strlen(c); j++)
+		{
+			if (c[j] > 65 || c[j] < 90)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(c);
+		c++;
+	}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+
+	return (0);
+}
