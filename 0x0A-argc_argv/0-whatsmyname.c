7fdfd2a450ac7b34ab319d0dd03f3343f6a328a7
nourister <nouristerjuma@gmail.com>
2023-05-12 00:54:45 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - that prints its name, followed by a new line.
+ * @argc: counts number of arguments in a cmd line
+ * @argv: size of an array of string
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
