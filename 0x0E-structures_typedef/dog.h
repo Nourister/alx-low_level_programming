7af1e9f2651edd2a853f75b004b74a62035bfc5d
nourister <nouristerjuma@gmail.com>
2023-05-22 07:25:32 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,7 +3,6 @@
 
 /**
  * struct dog - initialise a variable type struct dog
- * @d: pointer to struct dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: dog owner
@@ -16,15 +15,6 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
-
-/**
- * init_dog - initialises the struct dog
- * new_dog - creates new dog
- * _putchar - print the struct dog
- * @c: the character value
- * Return: value
- */
-/* description of other prototype */
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
