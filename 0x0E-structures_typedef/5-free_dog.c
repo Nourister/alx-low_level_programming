93597396e66895d8374e35b8760e1bcbb1f9f444
nourister <nouristerjuma@gmail.com>
2023-05-22 07:13:23 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include"dog.h"
+
+/**
+ * free_dog - free the dog
+ * @d: pointer to the struct dog
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if(d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
