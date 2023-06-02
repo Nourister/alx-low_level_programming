0ea1fecd730db49b8b8aa4d3bab6f96de90286f0
nourister <nourusterjuma@gmail.com>
2023-06-02 05:39:51 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-void print_message() __attribute__((constructor));
+void print_message(void) __attribute__((constructor));
 
 /**
  * print_message - prints before the main
- * 
  * Return: 0(success)
  */
 
-void print_message()
+void print_message(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
