e433a99fbd4994149054969a0321be000d57f0fa
nourister <nourusterjuma@gmail.com>
2023-07-21 12:48:37 -0700

    update

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 
-largest_palindrome = 0
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
 
-for i in range(100, 1000):
-    for n in range(100, 1000):
-        product = i * n
-        if str(product) == str(product)[::-1] and product > largest_palindrome:
-            largest_palindrome = product
+def find_largest_palindrome():
+    largest_palindrome = 0
+    for i in range(100, 1000):
+        for j in range(i, 1000):
+            product = i * j
+            if is_palindrome(product) and product > largest_palindrome:
+                largest_palindrome = product
+    return largest_palindrome
 
-#Save the result in the file "102-result"
+# Find the largest palindrome
+result = find_largest_palindrome()
+
+# Save the result in the file "102-result"
 with open("102-result", "w") as file:
-    file.write(str(largest_palindrome))
+    file.write(str(result))
